bca7851166516dd5c233e70de8873c5124754a42
luizas <luizasanchez.la@gmail.com>
2021-10-20 14:12:32 -0400

    Added Q15 Move Zeros to the End

--- src/setup/Q15Array.java
+++ src/setup/Q15Array.java
@@ -2,22 +2,25 @@ package setup;
 
 public class Q15Array {
 
-    public static void moveZeroes(int[] arr) {
+        /*
+        I wrote a code to move all zeroes to end of array in-place (without using any extra space). Basically, we are going to write a code which shift all zeros to right of array.
+         */
 
-        int len = arr.length;
+    public static void moveZeroes(int[] arr) { // Created Void method, this method doesn't return a value
+
         int count = 0;
 
-        for(int i = 0; i < len; i++) {
+        for(int i = 0; i < arr.length; i++) { // in first loop checks how many 0 have i arr length and counted line 14
             if(arr[i] != 0) {
                 arr[count++] = arr[i];
             } }
 
         //Now, remaining element is zero.
-        while(count < len) {
+        while(count < arr.length) {
             arr[count++] = 0; }
 
-        for(int j = 0; j < len; j++) {
-            System.out.print(arr[j] + " ");
+        for(int j = 0; j < arr.length; j++) { //
+            System.out.print(arr[j]);
         } }
     public static void main(String[] args) {
 
